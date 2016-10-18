5f8b6993d127cf3e7b57e1f5792ab6d3908c60a9
Nikita Tchayka <nikitatchayka@gmail.com>
2016-10-18 19:03:04 +0100

    Make basic login screen

--- src/View.elm
+++ src/View.elm
@@ -1,8 +1,19 @@
 module View exposing (..)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
 import Common.Model exposing (Model)
 import Update exposing (Msg)
 
 view : Model -> Html Msg
-view model = div [] [ text "Hello world" ]
+view model =
+  div [ class "valign-wrapper"]
+      [ div [ class "valign center-block" ]
+            [ input [ type' "text", placeholder "User" ]
+                    []
+            , input [ type' "password" ]
+                    []
+            , a [ class "waves-effect waves-light btn" ]
+                [ text "Login" ]
+            ]
+      ]
