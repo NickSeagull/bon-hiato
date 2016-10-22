4a4e805a1029dda52340b28ca5bf12384880758f
Nikita Tchayka <nikitatchayka@gmail.com>
2016-10-22 14:18:38 +0100

    Use the materialize modules

--- src/ProductOwner/View.elm
+++ src/ProductOwner/View.elm
@@ -1,5 +1,22 @@
 module ProductOwner.View exposing (..)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Model exposing (..)
+import Materialize.Materialize as Materialize
 
-view model = text "ProductOwner"
+view : Model -> Html msg
+view model =
+    Materialize.container []
+              [ Materialize.row []
+                    [ Materialize.col "s12" 
+                          []
+                          [ p [ class "dashboard-title" ] 
+                              [ text <| greetUser model ]
+                          ]
+                    ]
+              ]
+
+greetUser : Model -> String
+greetUser model =
+    model.username ++ " - Dashboard"
